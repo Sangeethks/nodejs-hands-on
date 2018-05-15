81dc804991f97e777468f7e03c75f94dc5dc4f61
Sangeethks <sangeethks.in@gmail.com>
2018-05-15 09:05:36 +0530

    Working with os module

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const os = require('os');
 
-const dataToAppend = 'Hello Sangeeth!';
+const user = os.userInfo();
+console.log('[user ]', user);
+
+const dataToAppend = `Hello ${user.username}`;
 fs.appendFile('greetings.txt', dataToAppend, err => {
   if (err) return console.log(`Error appending '${dataToAppend}' to the file`);
 
