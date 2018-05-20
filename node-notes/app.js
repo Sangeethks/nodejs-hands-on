ce0382fa937a783501d06e170fe26ae2a23d0d8e
Sangeethks <sangeethks.in@gmail.com>
2018-05-20 09:58:01 +0530

    Adding user data

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,4 +1,6 @@
 const yargs = require('yargs');
+// Import notes module
+const notes = require('./notes');
 
 // Get the argument from the terminal
 let argv = yargs.argv;
@@ -6,3 +8,17 @@ let argv = yargs.argv;
 let command = argv._[0];
 
 console.log('[Commands]', command);
+
+
+// Execute methods according to different commands
+switch (command) {
+  case 'add':
+    // Get the arguments
+    let title = argv.title;
+    let body = argv.body;
+    
+    notes.addNote(title, body);
+    break;
+  default:
+
+}
--- node-notes/notes.js
+++ node-notes/notes.js
@@ -1,3 +1,10 @@
+// Import file system module
+const fs = require('fs');
+// Import OS module
+const os = require('os');
+// User data
+let user = os.userInfo();
+
 const notes = {
   addNote: (title, body) => {
     if (!title) return console.log('Missing title');
@@ -7,6 +14,30 @@ const notes = {
 
     console.log('[notes addNote title]', title);
     console.log('[notes addNote body]', body);
+
+    // // Check for notes existence
+    // let notes = fs.readFile('./notes.json', (err, data) => {
+    //   // If error, exit out
+    //   if (err) return console.log('Error getting notes', err);
+    //
+    //   console.log('[addNote readFile data]', data);
+    //
+    //   let notes = data.notes;
+    // });
+
+    // let notes = [{
+    //   title, body
+    // }];
+    // let _info = {
+    //   created: new Date().getTime(),
+    //   updated: new Date().getTime(),
+    //   author: os.
+    // }
+    // let data = {
+    //
+    // }
+
+    // fs.appendFile()
   }
 }
 
